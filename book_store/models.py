9ec3e8c3617555009c4ab2b45c243d19bcf113fa
Aiup <64644481+Ayup923@users.noreply.github.com>
2022-11-30 13:42:30 +0600

    Update models.py

--- book_store/models.py
+++ book_store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+ 
 class Book(models.Model):
     category = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
